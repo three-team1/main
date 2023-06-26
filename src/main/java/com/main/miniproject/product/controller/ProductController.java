5de5a845d1449878f0b0a5c0cd8f0be0170ea131
dydtns1324 <dydtns1324@naver.com>
2023-06-26 10:02:21 +0900

    커밋 동기화

--- src/main/java/com/main/miniproject/product/controller/ProductController.java
+++ src/main/java/com/main/miniproject/product/controller/ProductController.java
@@ -1,17 +1,9 @@
 package com.main.miniproject.product.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.main.miniproject.product.entity.Product;
-import com.main.miniproject.product.entity.RealTimeSearch;
-import com.main.miniproject.product.service.ProductService;
 import com.main.miniproject.product.service.RealTimeSearchService;
 
 import lombok.extern.log4j.Log4j2;
--- src/main/java/com/main/miniproject/product/controller/ProductRestController.java
+++ src/main/java/com/main/miniproject/product/controller/ProductRestController.java
@@ -3,12 +3,10 @@ package com.main.miniproject.product.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.main.miniproject.product.entity.Product;
@@ -62,4 +60,3 @@ public class ProductRestController {
 	    return productService.searchProducts(searchKeyword);
 	}
 }
-
