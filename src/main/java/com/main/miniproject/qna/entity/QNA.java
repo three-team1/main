1cf8585b2a56cb097523a4298e1cd984fde0947f
eseulssi <eeeeeseulllll@naver.com>
2023-06-27 17:45:54 +0900

    이슬 머지 완료

--- src/main/java/com/main/miniproject/qna/entity/QNA.java
+++ src/main/java/com/main/miniproject/qna/entity/QNA.java
@@ -1,20 +1,12 @@
 package com.main.miniproject.qna.entity;
 
-import java.time.LocalDateTime;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-
 import com.main.miniproject.user.entity.User;
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
 
 @Entity
 @Data
@@ -30,12 +22,18 @@ public class QNA {
 	
 	@Column(name = "qna_title")
 	private String qnaTitle;
+
+	@Column(name = "qna_writer")
+	private String qnaWriter;
 	
 	@Column(name = "qna_content")
 	private String qnaContent;
 	
 	@Column(name = "qna_regdate")
 	private LocalDateTime qnaRegdate = LocalDateTime.now();
+
+	@Column(name="qna_Cnt", nullable = false)
+	private int qnaCnt = 0;
 	
 	@Column(name = "qna_response")
 	private String qnaResponse;
@@ -46,5 +44,10 @@ public class QNA {
 	@ManyToOne
 	@JoinColumn(name = "user_id")
 	private User user;
-
+	
+	
+	
+	
+	
+	
 }
