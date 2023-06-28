b98e8b4cc48524e9aaccdc3a9bde07c6254b8b76
Jay3404 <111181171+Jay3404@users.noreply.github.com>
2023-06-28 16:47:57 +0900

    장바구니 게시판 DB 연동

--- src/main/java/com/main/miniproject/cart/repository/CartRepository.java
+++ src/main/java/com/main/miniproject/cart/repository/CartRepository.java
@@ -1,4 +1,9 @@
 package com.main.miniproject.cart.repository;
 
-public interface CartRepository {
+import com.main.miniproject.cart.entity.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CartRepository extends JpaRepository<Cart, Long> {
 }
