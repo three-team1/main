1c80092d3c94ff61aea38d6766c11df887028d89
stats-dev <hohun94@naver.com>
2023-06-28 14:18:19 +0900

    me page updates

--- src/main/java/com/main/miniproject/user/controller/MypageViewController.java
+++ src/main/java/com/main/miniproject/user/controller/MypageViewController.java
@@ -1,14 +1,20 @@
 package com.main.miniproject.user.controller;
 
+import com.main.miniproject.board.entity.Board;
 import com.main.miniproject.user.entity.User;
 import com.main.miniproject.user.service.UserInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/mypage")
@@ -36,4 +42,28 @@ public class MypageViewController {
         model.addAttribute("user", user);
         return "mypage/myUpdate";
     }
+    @GetMapping("/myBoard")
+    public String myBoardView() {
+
+        return "mypage/myboard";
+    }
+
+    /*@GetMapping("/board/list")
+    public String getBoardList(Model model,
+                               @RequestParam(required = false) String keyword,
+                               @PageableDefault(size = 15 , sort = "id", direction = Sort.Direction.DESC) Pageable pageable) {
+
+        Page<Board> boardPage;
+        if(keyword != null) {
+            boardPage = boardService.searchBoard(pageable, keyword);
+        } else {
+            boardPage = boardService.getAllBoards(pageable);
+        }
+
+        model.addAttribute("boards", boardPage);
+        model.addAttribute("page", boardPage);
+
+        return "boardList";
+    }*/
+
 }
