ca596bd4f9fb8d6553f8bd9fdbb2834c2a0bb4a8
Jay3404 <111181171+Jay3404@users.noreply.github.com>
2023-06-29 15:58:08 +0900

    충돌 해결

--- src/main/java/com/main/miniproject/cart/repository/CartRepository.java
+++ src/main/java/com/main/miniproject/cart/repository/CartRepository.java
@@ -3,6 +3,7 @@ package com.main.miniproject.cart.repository;
 import com.main.miniproject.cart.entity.Cart;
 import com.main.miniproject.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
