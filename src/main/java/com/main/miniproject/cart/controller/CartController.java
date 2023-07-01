465eba8d484cdf004e451c8e29221bf5b0b50ffe
Jay3404 <111181171+Jay3404@users.noreply.github.com>
2023-07-01 20:24:22 +0900

    장바구니 중복 담기 및 장바구니 화면단 수정

--- src/main/java/com/main/miniproject/cart/controller/CartController.java
+++ src/main/java/com/main/miniproject/cart/controller/CartController.java
@@ -1,32 +1,32 @@
 package com.main.miniproject.cart.controller;
 
-import com.main.miniproject.cart.entity.Cart;
-import com.main.miniproject.cart.service.CartService;
-import com.main.miniproject.product.entity.Product;
-import com.main.miniproject.product.repository.ProductRepository;
-import com.main.miniproject.qna.service.QnaService;
-import com.main.miniproject.user.entity.User;
-import com.main.miniproject.user.repository.UserRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
+import java.util.List;
+
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpSession;
-import java.util.List;
+import com.main.miniproject.cart.entity.Cart;
+import com.main.miniproject.cart.service.CartService;
+import com.main.miniproject.product.entity.Product;
+import com.main.miniproject.product.repository.ProductRepository;
+import com.main.miniproject.user.entity.User;
+import com.main.miniproject.user.repository.UserRepository;
+import com.main.miniproject.user.service.UserDetail;
+
+import lombok.RequiredArgsConstructor;
 
 
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping
 public class CartController {
+
     private final CartService cartService;
 
     private final UserRepository userRepository;
@@ -34,9 +34,11 @@ public class CartController {
     private final ProductRepository productRepository;
 
     @GetMapping("/cart")
-    public String cart(Model model, @AuthenticationPrincipal UserDetails userDetails) {
+    public String cart(Model model, @AuthenticationPrincipal UserDetail userDetail) {
 
-            User user = userRepository.findByUsername(userDetails.getUsername()).get();
+            //User user = userRepository.findByUsername(userDetails.getUsername()).get(); // 재민님로직
+    	
+            User user = userRepository.findById(userDetail.getUser().getId()).get(); // 변경사항 ()
 
             List<Cart> cartList = cartService.getList(user);
             model.addAttribute("cartList", cartList);
@@ -44,26 +46,27 @@ public class CartController {
         return "/cart/cart";
     }
 
-    @PostMapping("/insertCart/{id}")
-    public void insertCart(@PathVariable Long id, @AuthenticationPrincipal UserDetails userDetails) {
-
-        User user = userRepository.findByUsername(userDetails.getUsername()).get();
-        Product product = productRepository.findById(id).get();
-
-        Cart cart = Cart.builder()
-                .product(product)
-                .user(user)
-                .cartQuantity(1)
-                .build();
-
-        cartService.insertCart(cart);
-
-    }
-
-    @PostMapping("/plusQuantityCart")
-    public void plusQuantityCart(){
-
-    }
+    
+    // 해당메소드 RestController로 이동
+    
+//    @PostMapping("/insertCart/{id}")
+//    public void insertCart(@PathVariable Long id, @AuthenticationPrincipal UserDetail userDetail) {
+//
+//        // User user = userRepository.findByUsername(userDetails.getUsername()).get(); // 재민님 로직
+//    	
+//    	User user = userRepository.findById(userDetail.getUser().getId()).get();  	// 변경사항 ()
+//    	
+//        Product product = productRepository.findById(id).get();
+//
+//        Cart cart = Cart.builder()
+//                .product(product)
+//                .user(user)
+//                .cartQuantity(1)
+//                .build();
+//
+//        cartService.insertCart(cart);
+//
+//    }
 
     @GetMapping("/deleteCart/{id}")
     public String delCart(@PathVariable Long id) {
@@ -88,4 +91,3 @@ public class CartController {
 
 
 }
-
--- src/main/java/com/main/miniproject/cart/repository/CartRepository.java
+++ src/main/java/com/main/miniproject/cart/repository/CartRepository.java
@@ -1,15 +1,21 @@
 package com.main.miniproject.cart.repository;
 
-import com.main.miniproject.cart.entity.Cart;
-import com.main.miniproject.user.entity.User;
+import java.util.List;
+import java.util.Optional;
+
+import com.main.miniproject.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import com.main.miniproject.cart.entity.Cart;
+import com.main.miniproject.user.entity.User;
 
 @Repository
 @Transactional
 public interface CartRepository extends JpaRepository<Cart, Long> {
     List<Cart> findAllByUser(User user);
+
+    Cart findByProductAndUser(Product product, User user);
+    
 }
