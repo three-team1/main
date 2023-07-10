98bbcd341c50210d40ee56238699474f6ac52f93
eseulssi <eeeeeseulllll@naver.com>
2023-07-10 14:01:46 +0900

    충돌해결

--- src/main/java/com/main/miniproject/order/controller/OrderController.java
+++ src/main/java/com/main/miniproject/order/controller/OrderController.java
@@ -2,21 +2,26 @@ package com.main.miniproject.order.controller;
 
 import com.main.miniproject.cart.entity.Cart;
 import com.main.miniproject.cart.repository.CartRepository;
-import com.main.miniproject.order.entity.OrderItem;
-import com.main.miniproject.order.entity.Orders;
+
+
 import com.main.miniproject.order.orderItemRepository.OrderItemRepository;
 import com.main.miniproject.order.service.OrderService;
 import com.main.miniproject.user.repository.UserRepository;
-import com.main.miniproject.user.service.UserDetail;
+
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 @RequiredArgsConstructor
@@ -43,12 +48,5 @@ public class OrderController {
 
 		return "order/order";
 	}
-
-	@PostMapping("/insertOrders")
-	public String insertOrders(Orders orders){
-
-		return "";
-	}
-
 
 }
