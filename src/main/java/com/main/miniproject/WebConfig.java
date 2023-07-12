c23b6cda827fc0cbd271cad8a6b47383e9789664
zeee88 <jihae0808@naver.com>
2023-07-13 04:35:03 +0900

    0713

--- src/main/java/com/main/miniproject/WebConfig.java
+++ src/main/java/com/main/miniproject/WebConfig.java
@@ -1,5 +1,6 @@
 package com.main.miniproject;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,6 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
 
+	@Value("${itemImgLocation}")
+	String itemImgPath;
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
@@ -14,6 +17,7 @@ public class WebConfig implements WebMvcConfigurer{
 		registry.addResourceHandler("/board/images/**").addResourceLocations("file:/C:/board/images/");
 		registry.addResourceHandler("/review/images/**").addResourceLocations("file:/C:/review/images/");
 		registry.addResourceHandler("/review/resized/**").addResourceLocations("file:/C:/review/resized/");
+		registry.addResourceHandler("/src/main/resources/static/images/**").addResourceLocations("file:" + itemImgPath);
 	}
 
 }
--- src/main/java/com/main/miniproject/product/repository/ProductImageRepository.java
+++ src/main/java/com/main/miniproject/product/repository/ProductImageRepository.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.main.miniproject.product.entity.Product;
@@ -18,4 +20,7 @@ public interface ProductImageRepository extends JpaRepository<ProductImage, Long
 
 	List<ProductImage> findByName(ProductImage productImage);
 
+
+	@Query("SELECT P FROM ProductImage P WHERE P.product.id = :productId")
+	List<ProductImage> findProductImagesByProductId(@Param("productId")Long productId);
 }
