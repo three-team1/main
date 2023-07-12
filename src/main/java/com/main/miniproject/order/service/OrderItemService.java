2f8f56d08760236cb1558c0d3e9285f7158219ab
zeee88 <jihae0808@naver.com>
2023-07-12 12:35:48 +0900

    order table 수정

--- src/main/java/com/main/miniproject/order/service/OrderItemService.java
+++ src/main/java/com/main/miniproject/order/service/OrderItemService.java
@@ -1,35 +1,13 @@
 package com.main.miniproject.order.service;
 
 import com.main.miniproject.order.entity.OrderItem;
-<<<<<<< HEAD
+
 import com.main.miniproject.order.orderItemRepository.OrderItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
-
-@Service
-@RequiredArgsConstructor
-public class OrderItemService {
-
-    @Autowired
-    public OrderItemRepository orderItemRepository;
-
-    //orderItem 상세 정보 조회
-    public OrderItem getOrderItemDetail(Long id) {
-
-        Optional<OrderItem> orderItemOptional = orderItemRepository.findById(id);
-
-        if(orderItemOptional.isPresent()) {
-            OrderItem orderItem = orderItemOptional.get();
-            return orderItem;
-        } else{
-            return new OrderItem();
-        }
-
-    }
-=======
 import com.main.miniproject.order.entity.Orders;
 import com.main.miniproject.order.orderItemRepository.OrderItemRepository;
 import com.main.miniproject.order.repository.MypageOrdersRepository;
@@ -50,6 +28,24 @@ import java.util.List;
 @RequiredArgsConstructor
 @Data
 public class OrderItemService {
+
+//    @Autowired
+//    public OrderItemRepository orderItemRepository;
+
+    //orderItem 상세 정보 조회
+    public OrderItem getOrderItemDetail(Long id) {
+
+        Optional<OrderItem> orderItemOptional = orderItemRepository.findById(id);
+
+        if (orderItemOptional.isPresent()) {
+            OrderItem orderItem = orderItemOptional.get();
+            return orderItem;
+        } else {
+            return new OrderItem();
+        }
+
+    }
+
     private final OrderItemRepository orderItemRepository;
     private final MypageOrdersRepository mypageOrdersRepository;
     private final ProductImageRepository productImageRepository;
@@ -61,11 +57,12 @@ public class OrderItemService {
     public List<OrderItem> getProductsList(Long userId) {
         return orderItemRepository.findOrderItemsByUserId(userId);
     }
+
     public List<ProductImage> getImage(Long userId) {
         List<OrderItem> orderItemList = orderItemRepository.findOrderItemsByUserId(userId);
 
         List<ProductImage> productImageList = new ArrayList<>();
-        for(int i = 0; i < orderItemList.size(); i++){
+        for (int i = 0; i < orderItemList.size(); i++) {
             productImageList.add(productImageRepository.findByProduct(orderItemList.get(i).getProduct()).get(0));
         }
 
@@ -74,5 +71,5 @@ public class OrderItemService {
 
 
 //    주문내역은 삭제할 수 없다.
->>>>>>> a1b3e0f566c991622e0aac5138d4f613033b1dd1
+
 }
